0eeca4091b6261142d2a57985aecb1e3800bd200
Filip Swierczynski <swirek.gw@hotmail.com>
2023-04-04 12:32:28 -0400

    added bmi calculator with get and post requests

--- my-express-server/calculator/calculator.js
+++ my-express-server/calculator/calculator.js
@@ -1,10 +1,35 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/',(req,res)=>{
-    res.send('<h1>Calculator</h1>');
+    res.sendFile(__dirname + "/index.html");
 }); 
+app.get('/bmiCalculator',(req,res)=>{
+    res.sendFile(__dirname + "/bmiCalculator.html")
+})
+
+
+
+
+app.post('/',(req,res)=>{
+
+    let num1 = Number(req.body.num1);
+let num2 = Number(req.body.num2);
+
+let results = num1+num2;
+
+    res.send('thanks for sending ' + results);
+});
+
+app.post('/bmicalculator',(req,res)=>{
+    let num1 = Number(req.body.weight);
+    let num2 = Number(req.body.height);
+    let results = (num1 ) / (num2 * num2);
+    res.send('your bmi is' + results);
+})
 
 
 
